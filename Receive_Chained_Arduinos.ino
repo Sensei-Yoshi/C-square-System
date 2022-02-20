d399d2a8838676c8d8e595ade949511872fc863b
Sensei-Yoshi <94663295+Sensei-Yoshi@users.noreply.github.com>
2022-02-19 19:19:41 -0500

    Basic receiving number data to test chaining arduinos

---
+++ Receive_Chained_Arduinos.ino
@@ -0,0 +1,26 @@
+#include <EasyTransfer.h>
+
+//create object
+EasyTransfer ET;
+
+int mydata = 0;
+
+//give a name to the group of data
+
+
+void setup() {
+  Serial.begin(9600);
+  //start the library, pass in the data details and the name of the serial port. Can be Serial, Serial1, Serial2, etc.
+  ET.begin(details(mydata), &Serial);
+  
+}
+
+void loop() {
+  //check and see if a data packet has come in.
+  if (ET.receiveData()) {
+    Serial.println(mydata);
+  }
+
+  //you should make this delay shorter then your transmit delay or else messages could be lost
+  delay(250);
+}
